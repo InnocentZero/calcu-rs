3c63c1eec7c33dfbc48e84c0f5f0b96f4a93a172
innocentzero <isfarulhaque@proton.me>
2024-06-10 10:12:47 +0530

    feat(main): create/parse config file/cli options
    Signed-off-by: innocentzero <isfarulhaque@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,70 @@
-mod config;
+use std::{
+    fs::{create_dir_all, File},
+    io::{self, Result},
+    path::{Path, PathBuf},
+};
 
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+mod config;
+use config::_Config;
+
+use crate::config::get_config_path;
+
+/// A command-line journal logger, scheduler and task manager.
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to config-file
+    #[arg(short, long)]
+    config: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let config_file = match args.config {
+        Some(path) => Ok(path),
+        None => get_config_path(),
+    }
+    .map_err(|e| {
+        eprintln!("Error encountered: {e:?}");
+        io::ErrorKind::NotFound
+    })?;
+
+    if !config_file.exists() {
+        create_dir_all(
+            config_file
+                .parent()
+                .unwrap_or(Path::new("/home/nobody_has_this_as_home_dir")),
+        )
+        .map_err(|e| {
+            eprintln!("Failed to create parent directories");
+            eprintln!("{e:?}");
+            io::ErrorKind::NotFound
+        })?;
+
+        {
+            let file = File::create(&config_file).map_err(|e| {
+                eprintln!("Error occured: {e:?}");
+                io::ErrorKind::NotFound
+            })?;
+
+            file.sync_all().map_err(|e| {
+                eprintln!("Error occured: {e:?}");
+                io::ErrorKind::NotFound
+            })?;
+        }
+
+        config::write_default_config(&config_file).map_err(|e| {
+            eprintln!("Error occured while writing the default config.");
+            eprintln!("{e:?}");
+            io::ErrorKind::InvalidInput
+        })?;
+    }
+
+    let config = _Config::try_parse(&config_file).map_err(|e| {
+        eprintln!("Error occured in reading config file!");
+        eprintln!("{e:?}");
+        io::ErrorKind::NotFound
+    })?;
+    todo!();
 }
