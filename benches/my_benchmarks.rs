e2563ae874a47c011c4ad9e47d9deb8e6fc8bc6f
innocentzero <isfarulhaque@proton.me>
2024-06-29 01:57:06 +0530

    test: Add more testcases
    Signed-off-by: innocentzero <isfarulhaque@proton.me>

--- benches/my_benchmarks.rs
+++ benches/my_benchmarks.rs
@@ -8,7 +8,52 @@ pub fn parse_sequence_benchmark(c: &mut Criterion) {
     let start_date = NaiveDate::from_ymd_opt(2024, 6, 1).unwrap();
     let end_date = NaiveDate::from_ymd_opt(2024, 6, 30).unwrap();
 
-    c.bench_function("parse_sequence", |b| {
+    c.bench_function("parse_sequence 20", |b| {
+        b.iter(|| {
+            parse::parse_sequence(
+                black_box(&start_date),
+                black_box(&end_date),
+                black_box(PathBuf::from_str("tests").as_mut().unwrap()),
+            )
+        })
+    });
+}
+
+pub fn parse_sequence_benchmark_100(c: &mut Criterion) {
+    let start_date = NaiveDate::from_ymd_opt(2024, 8, 1).unwrap();
+    let end_date = NaiveDate::from_ymd_opt(2024, 8, 30).unwrap();
+
+    c.bench_function("parse_sequence 100", |b| {
+        b.iter(|| {
+            parse::parse_sequence(
+                black_box(&start_date),
+                black_box(&end_date),
+                black_box(PathBuf::from_str("tests").as_mut().unwrap()),
+            )
+        })
+    });
+}
+
+pub fn parse_sequence_benchmark_300(c: &mut Criterion) {
+    let start_date = NaiveDate::from_ymd_opt(2024, 1, 1).unwrap();
+    let end_date = NaiveDate::from_ymd_opt(2024, 1, 30).unwrap();
+
+    c.bench_function("parse_sequence 300", |b| {
+        b.iter(|| {
+            parse::parse_sequence(
+                black_box(&start_date),
+                black_box(&end_date),
+                black_box(PathBuf::from_str("tests").as_mut().unwrap()),
+            )
+        })
+    });
+}
+
+pub fn parse_sequence_benchmark_700(c: &mut Criterion) {
+    let start_date = NaiveDate::from_ymd_opt(2024, 9, 1).unwrap();
+    let end_date = NaiveDate::from_ymd_opt(2024, 9, 30).unwrap();
+
+    c.bench_function("parse_sequence 700", |b| {
         b.iter(|| {
             parse::parse_sequence(
                 black_box(&start_date),
@@ -23,7 +68,7 @@ pub fn parse_sequence_benchmark_long_file(c: &mut Criterion) {
     let start_date = NaiveDate::from_ymd_opt(2024, 7, 1).unwrap();
     let end_date = NaiveDate::from_ymd_opt(2024, 7, 30).unwrap();
 
-    c.bench_function("parse_sequence_long", |b| {
+    c.bench_function("parse_sequence 3000", |b| {
         b.iter(|| {
             parse::parse_sequence(
                 black_box(&start_date),
@@ -37,6 +82,9 @@ pub fn parse_sequence_benchmark_long_file(c: &mut Criterion) {
 criterion_group!(
     benches,
     parse_sequence_benchmark,
+    parse_sequence_benchmark_100,
+    parse_sequence_benchmark_300,
+    parse_sequence_benchmark_700,
     parse_sequence_benchmark_long_file
 );
 criterion_main!(benches);
